2f702c32503c6b0c390788b28b9ee7a919c81cc1
firalabs <firalabs@gmail.com>
2012-12-18 18:04:20 -0500

    add base tests

---
+++ tests/GeneratorsTest.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Unit tests for generator
+ * @author maxime.beaudoin
+ *
+ */
+class GeneratorsTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testExample()
+	{
+		
+		$this->assertTrue(true);
+	
+	}
+
+}
